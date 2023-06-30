c4ccad0fd6f716749f611a672498bdde32efe0bd
Brian Chang <viere0928@gmail.com>
2023-06-30 21:39:44 +0800

    Revert "Initial Project"
    This reverts commit 62f990e1e95f2b0010a5b0be9b6aa856ae0ac6b8.

--- Pods/Target Support Files/MatrixSDK/MatrixSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MatrixSDK : NSObject
-@end
-@implementation PodsDummy_MatrixSDK
-@end
--- Pods/Target Support Files/MatrixSDKCrypto/MatrixSDKCrypto-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MatrixSDKCrypto : NSObject
-@end
-@implementation PodsDummy_MatrixSDKCrypto
-@end
